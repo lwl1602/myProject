ffdfdef40f266e4cbfe5ac98f1ecb62649e53c22
root <1025645008@qq.com>
2017-04-01 17:02:19 +0800

    write blog login

--- blog/app/Http/Controllers/Admin/LoginController.php
+++ blog/app/Http/Controllers/Admin/LoginController.php
@@ -1,12 +1,34 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
 
-class LoginController extends Controller
+require_once 'E:/mygit/myProject/blog/resources/org/code/Code.class.php';
+
+class LoginController extends CommonController
 {
-    //
+    public function login(){
+
+        if($input = Input::all()){//Input::all() 是否提交了数据
+            $code = new \Code;
+            $code->get();
+            if($input['code'] != $code){
+                return back()->with('msg','验证码错误');      //返回到请求页面,后面加with调教验证码
+            }
+
+        }else{
+            return view('admin.login');
+        }
+    }
+
+    public function code(){
+        $code = new \Code;
+        $code->make();
+    }
+
 }
+
--- blog/resources/views/admin/login.blade.php
+++ blog/resources/views/admin/login.blade.php
@@ -2,29 +2,32 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="style/css/ch-ui.admin.css">
-	<link rel="stylesheet" href="style/font/css/font-awesome.min.css">
+	<link rel="stylesheet" href="{{ asset('style/css/ch-ui.admin.css') }}">
+	<link rel="stylesheet" href="{{ asset('style/font/css/font-awesome.min.css') }}">
 </head>
 <body style="background:#F3F3F4;">
 	<div class="login_box">
-		<h1>Blog</h1>
+		<h1>LOGIN VIEW</h1>
 		<h2>欢迎使用博客管理平台</h2>
 		<div class="form">
-			<p style="color:red">用户名错误</p>
+			@if(session('msg'))
+				<p style="color:red">{{ session('msg') }}</p>
+				@endif
 			<form action="#" method="post">
+				{{ csrf_field() }}  {{--验证token--}}
 				<ul>
 					<li>
-					<input type="text" name="username" class="text"/>
+					<input type="text" name="user_name" class="text"/>
 						<span><i class="fa fa-user"></i></span>
 					</li>
 					<li>
-						<input type="password" name="password" class="text"/>
+						<input type="text" name="user_pass" class="text"/>
 						<span><i class="fa fa-lock"></i></span>
 					</li>
 					<li>
 						<input type="text" class="code" name="code"/>
 						<span><i class="fa fa-check-square-o"></i></span>
-						<img src="#" alt="">
+						<img src="{{ url('admin/code') }}" alt="" onclick="this.src='{{ url('admin/code') }}?'+Math.random()">
 					</li>
 					<li>
 						<input type="submit" value="立即登陆"/>
